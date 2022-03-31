f7461ec2d39eb7266f04bd7f612cd44eda09ccca
AlexTsoy <Alexandrotsoy@yandex.ru>
2022-03-31 17:52:37 +0300

    Task6. First push.

--- src/main/java/DAO.java
+++ src/main/java/DAO.java
@@ -1,12 +1,12 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.ParseException;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class DAO {
+
     public Map<String,String> startLapDAO() throws IOException {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC/Greenwich"));
         Stream<String> start = Files.lines(Paths.get("start.log"));
@@ -14,18 +14,17 @@ public class DAO {
         return startLap;
     }
 
-    public Map<String,String> endLapDAO() throws IOException, ParseException {
+    public Map<String,String> endLapDAO() throws IOException {
         Stream<String> end = Files.lines(Paths.get("end.log"));
         Map<String, String> endLap = end.distinct().collect(Collectors.toMap((p) -> p.substring(0, 3), (p) -> p.substring(14)));
         return endLap;
     }
 
-    public Map<String, List<String>> abbreviationsDAO() throws IOException, ParseException {
+    public Map<String, List<String>> abbreviationsDAO() throws IOException {
         Stream<String> abbreviations = Files.lines(Paths.get("abbreviations.txt"));
         Map<String, List<String>> abbreviationsMap = abbreviations.distinct().collect(Collectors.toMap(
                 (p) -> p.substring(0, 3),
                 (p) -> Arrays.stream(p.substring(4).split("_")).toList()));
         return abbreviationsMap;
     }
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import java.io.IOException;
-import java.text.ParseException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ParseException {
+
+    public static void main(String[] args) throws IOException {
         DAO dao = new DAO();
         LapTimeCalculate calculate = new LapTimeCalculate();
-        calculate.calculate(dao.startLapDAO(), dao.endLapDAO());
-        System.out.println(dao.abbreviationsDAO());
+        Formatter formatter = new Formatter();
+        StringBuilder result = formatter.formatter(calculate.calculate(dao.startLapDAO(), dao.endLapDAO()), dao.abbreviationsDAO());
+        System.out.println(result);
     }
 }
